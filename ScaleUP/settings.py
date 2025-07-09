b82caf340e421a5fbe614c4a4ebdd51b9c20f14a
mhdimohammadi <mohammadmahdi11121382@gmail.com>
2025-07-09 14:01:25 +0330

    updating settings

--- ScaleUP/settings.py
+++ ScaleUP/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
         'PORT': os.getenv('PORT'),
     }
 }
