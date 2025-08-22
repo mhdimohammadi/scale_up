98bf28c36302f0d3b446c01b0cdcb0206f3a600f
mhdimohammadi <mohammadmahdi11121382@gmail.com>
2025-08-22 22:13:13 +0330

    editing readme

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-for testing you can run the python manage.py test inside the container.
-also remember to run python manage.py makemigrations and migrate in the container so the tables are created.
-for building image using the dockerfile and docker-compose please and 'HOST': 'db' to the DATABASES in the settings.py 
-it should look like this :
+remember to run python manage.py makemigrations and migrate in the container so the tables are created.
+for building image using the dockerfile and docker-compose please change the host to 'HOST': 'db' in the DATABASES in 
+the settings.py, it should look like this :
 ```
 DATABASES = {
     'default': {
@@ -14,4 +13,14 @@ DATABASES = {
     }
 }
 ```
-git hub : https://github.com/mhdimohammadi/scale_up
+urls :
+ ```
+ urlpatterns = [
+    path('flags/',views.FlagListView.as_view(),name='flag_list'), #GET
+    path('flags/<int:pk>',views.FlagDetailView.as_view(),name='flag_detail'), #GET
+    path('toggle/<int:pk>',views.FlagToggleView.as_view(),name='flag_toggle'), #POST
+    path('add_flag',views.AddFlagView.as_view(),name='add_flag'), #POST
+    path('add_dependency',views.AddDependencyView.as_view(),name='add_dependency'), #POST
+    path('logs',views.AuditLogListView.as_view(),name='logs'), #GET
+]
+ ```
--- flag/views.py
+++ flag/views.py
@@ -3,50 +3,52 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from flag.serializers import FlagSerializer, FlagDependencySerializer, AuditLogSerializer
-from .models import Flag,AuditLog
+from .models import Flag, AuditLog
 from .services import auto_disable_dependents_cascade
+
 
 class FlagListView(APIView):
     def get(self, request):
         instance = Flag.objects.all()
-        serializer = FlagSerializer(instance,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        serializer = FlagSerializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class FlagDetailView(APIView):
-   def get(self,request,pk):
-       instance = get_object_or_404(Flag, pk=pk)
-       serializer = FlagSerializer(instance)
-       return Response(serializer.data,status=status.HTTP_200_OK)
+    def get(self, request, pk):
+        instance = get_object_or_404(Flag, pk=pk)
+        serializer = FlagSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class FlagToggleView(APIView):
 
-
-
-    def post(self,request,pk):
+    def post(self, request, pk):
         instance = get_object_or_404(Flag, pk=pk)
         if instance.is_active:
             auto_disable_dependents_cascade(instance)
             instance.is_active = False
             instance.save()
-            AuditLog.objects.create(flag=instance,action='toggle_off',reason='manual inactive',actor='API')
-        else :
-            inactive_deps = [dep.depends_on.name for dep in instance.dependencies.all() if dep.depends_on.is_active == False]
+            AuditLog.objects.create(flag=instance, action='toggle_off', reason='manual inactive', actor='API')
+        else:
+            inactive_deps = [dep.depends_on.name for dep in instance.dependencies.all() if
+                             dep.depends_on.is_active == False]
             if inactive_deps:
-                return Response({"error":"inactive dependencies",
-                                 "inactive dependencies": inactive_deps},status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "inactive dependencies",
+                                 "inactive dependencies": inactive_deps}, status=status.HTTP_400_BAD_REQUEST)
             instance.is_active = True
             instance.save()
             AuditLog.objects.create(flag=instance, action='toggle_on', reason='manual active', actor='API')
-        return Response({"response":f'{instance.name} is now {"active" if instance.is_active else "inactive"}'},status=status.HTTP_200_OK)
+        return Response({"response": f'{instance.name} is now {"active" if instance.is_active else "inactive"}'},
+                        status=status.HTTP_200_OK)
 
 
 class AddFlagView(APIView):
-    def post(self,request):
+    def post(self, request):
         serializer = FlagSerializer(data=request.data)
         if serializer.is_valid():
             flag = serializer.save()
-            AuditLog.objects.create(flag=flag,action='create',reason=f"Flag {flag.name} was created",actor='API')
+            AuditLog.objects.create(flag=flag, action='create', reason=f"Flag {flag.name} was created", actor='API')
             return Response({"flag added"}, status=status.HTTP_201_CREATED)
         errors = []
         for field, messages in serializer.errors.items():
@@ -55,21 +57,20 @@ class AddFlagView(APIView):
                     "field": field,
                     "message": str(message)
                 })
-        return Response({"errors":errors}, status=status.HTTP_400_BAD_REQUEST)
-
+        return Response({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AddDependencyView(APIView):
-    def post(self,request):
+    def post(self, request):
         serializer = FlagDependencySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"dependency added"}, status=status.HTTP_201_CREATED)
-        return Response({"errors":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AuditLogListView(APIView):
     def get(self, request):
         instance = AuditLog.objects.all()
-        serializer = AuditLogSerializer(instance,many=True)
+        serializer = AuditLogSerializer(instance, many=True)
         return Response(serializer.data)
